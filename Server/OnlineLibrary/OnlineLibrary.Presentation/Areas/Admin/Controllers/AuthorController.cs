436b226f75992a5813a2684770706ca687d89e9a
KiSSel3 <andreyk042004@gmail.com>
2024-08-14 14:09:28 +0300

    Added author controller by admin

---
+++ Server/OnlineLibrary/OnlineLibrary.Presentation/Areas/Admin/Controllers/AuthorController.cs
@@ -0,0 +1,48 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using OnlineLibrary.BLL.DTOs.Common;
+using OnlineLibrary.BLL.DTOs.Request.Author;
+using OnlineLibrary.BLL.UseCases.Interfaces.Author;
+
+namespace OnlineLibrary.Presentation.Areas.Admin.Controllers;
+
+[ApiController]
+[Authorize(Policy = "AdminArea")]
+[Area("admin")]
+[Route("/api/author")]
+public class AuthorController : ControllerBase
+{
+    private readonly ICreateNewAuthorUseCase _createNewAuthorUseCase;
+    private readonly IDeleteAuthorUseCase _deleteAuthorUseCase;
+    private readonly IUpdateAuthorUseCase _updateAuthorUseCase;
+
+    public AuthorController(ICreateNewAuthorUseCase createNewAuthorUseCase, IDeleteAuthorUseCase deleteAuthorUseCase, IUpdateAuthorUseCase updateAuthorUseCase)
+    {
+        _createNewAuthorUseCase = createNewAuthorUseCase;
+        _deleteAuthorUseCase = deleteAuthorUseCase;
+        _updateAuthorUseCase = updateAuthorUseCase;
+    }
+    
+    [HttpPost("create")]
+    public async Task<IActionResult> CreateAsync([FromBody] AuthorDTO authorDTO, CancellationToken cancellationToken = default)
+    {
+        await _createNewAuthorUseCase.ExecuteAsync(authorDTO, cancellationToken);
+        return Ok();
+    }
+    
+    [HttpPut("update")]
+    public async Task<IActionResult> UpdateAsync([FromBody] AuthorUpdateRequestDTO authorUpdateRequestDTO, CancellationToken cancellationToken = default)
+    {
+        await _updateAuthorUseCase.ExecuteAsync(authorUpdateRequestDTO,cancellationToken);
+        return Ok();
+    }
+    
+    [HttpDelete("delete/{id}")]
+    public async Task<IActionResult> DeleteAsync(Guid id, CancellationToken cancellationToken = default)
+    {
+        await _deleteAuthorUseCase.ExecuteAsync(id, cancellationToken);
+        return Ok();
+    }
+}
+
