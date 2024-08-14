9e88a3ccc8d4812d41d3bf08e19f4e7142d1990c
KiSSel3 <andreyk042004@gmail.com>
2024-08-14 14:18:03 +0300

    Added genre controller by admin

---
+++ Server/OnlineLibrary/OnlineLibrary.Presentation/Areas/Admin/Controllers/GenreController.cs
@@ -0,0 +1,45 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using OnlineLibrary.BLL.DTOs.Common;
+using OnlineLibrary.BLL.UseCases.Interfaces.Genre;
+
+namespace OnlineLibrary.Presentation.Areas.Admin.Controllers;
+
+[ApiController]
+[Authorize(Policy = "AdminArea")]
+[Area("admin")]
+[Route("/api/genre")]
+public class GenreController : Controller
+{
+    private readonly ICreateNewGenreUseCase _createNewGenreUseCase;
+    private readonly IDeleteGenreUseCase _deleteGenreUseCase;
+    private readonly IUpdateGenreUseCase _updateGenreUseCase;
+
+    public GenreController(ICreateNewGenreUseCase createNewGenreUseCase, IDeleteGenreUseCase deleteGenreUseCase, IUpdateGenreUseCase updateGenreUseCase)
+    {
+        _createNewGenreUseCase = createNewGenreUseCase;
+        _deleteGenreUseCase = deleteGenreUseCase;
+        _updateGenreUseCase = updateGenreUseCase;
+    }
+    
+    [HttpPost("create/{name}")]
+    public async Task<IActionResult> CreateAsync(string name, CancellationToken cancellationToken = default)
+    {
+        await _createNewGenreUseCase.ExecuteAsync(name, cancellationToken);
+        return Ok();
+    }
+    
+    [HttpPut("update")]
+    public async Task<IActionResult> UpdateAsync([FromBody] GenreDTO genreDTO, CancellationToken cancellationToken = default)
+    {
+        await _updateGenreUseCase.ExecuteAsync(genreDTO,cancellationToken);
+        return Ok();
+    }
+    
+    [HttpDelete("delete/{id}")]
+    public async Task<IActionResult> DeleteAsync(Guid id, CancellationToken cancellationToken = default)
+    {
+        await _deleteGenreUseCase.ExecuteAsync(id, cancellationToken);
+        return Ok();
+    }
+}
